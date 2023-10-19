387bb4f1517dd9f7eaffadad70ab264d46b2b66d
Temerius <you@example.com>
2023-10-19 22:28:53 +0300

    maksim lox

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
-<script>
-            let draggedCard = null;
-        
-            function drag(event) {
-              draggedCard = event.target;
-            }
-        
-            function allowDrop(event) {
-              event.preventDefault();
-            }
-        
-            function drop(event, columnClass) {
-              event.preventDefault();
-              if (draggedCard) {
-                const targetColumn = document.querySelector(`.${columnClass}`);
-                targetColumn.appendChild(draggedCard);
-                draggedCard = null;
-              }
-            }
-        </script>
+
+let draggedCard = null;
+
+function drag(event) {
+    draggedCard = event.target;
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drop(event, columnClass) {
+    event.preventDefault();
+    if (draggedCard) {
+    const targetColumn = document.querySelector(`.${columnClass}`);
+    targetColumn.appendChild(draggedCard);
+    draggedCard = null;
+    }
+}
